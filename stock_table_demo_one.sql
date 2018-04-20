ba42b2854fa50dc5a01dd3c7dfadfd81cec21c42
Amy Brown <abrown@intersystems.com>
2018-04-20 09:50:47 -0400

    renamed loader for 20 record table

---
+++ stock_table_demo_one.sql
@@ -0,0 +1,63 @@
+CREATE TABLE FirstLook.StockTableDemoOne (ClientID INTEGER, BrokerID INTEGER,
+Symbol VARCHAR(10), TransactionType VARCHAR(4), TransactionDate TIMESTAMP, Quantity INTEGER, Price DECIMAL(15,2), CommmissionRate DECIMAL(15,2))
+GO
+INSERT INTO FirstLook.StockTableDemoOne (ClientID, BrokerID, Symbol, TransactionType, TransactionDate, Quantity, Price, CommmissionRate) 
+   VALUES (29834783, 3103, 'RTYU', 'SELL', '2016-01-03', 342, 5.05, 3.25)
+GO
+INSERT INTO FirstLook.StockTableDemoOne (ClientID, BrokerID, Symbol, TransactionType, TransactionDate, Quantity, Price, CommmissionRate) 
+   VALUES (59203943, 5001, 'ASUB', 'BUY',  '2017-12-13', 56, 15.62, 4.25)
+GO
+INSERT INTO FirstLook.StockTableDemoOne (ClientID, BrokerID, Symbol, TransactionType, TransactionDate, Quantity, Price, CommmissionRate) 
+   VALUES (29834783, 3103, 'FGQT', 'BUY',  '2016-01-14', 500, 32.41, 3.25)
+GO
+INSERT INTO FirstLook.StockTableDemoOne (ClientID, BrokerID, Symbol, TransactionType, TransactionDate, Quantity, Price, CommmissionRate) 
+   VALUES (10993023, 1009, 'YTR', 'SELL',  '2016-01-15', 50, 33.87, 5.25)
+GO
+INSERT INTO FirstLook.StockTableDemoOne (ClientID, BrokerID, Symbol, TransactionType, TransactionDate, Quantity, Price, CommmissionRate) 
+   VALUES (40149304, 5001, 'YQER', 'SELL',  '2015-11-13', 120, 109.28, 4.25)
+GO
+INSERT INTO FirstLook.StockTableDemoOne (ClientID, BrokerID, Symbol, TransactionType, TransactionDate, Quantity, Price, CommmissionRate) 
+   VALUES (37482093, 1009, 'TIOE', 'BUY',  '2018-03-01', 500, 56.23, 5.25)
+GO
+INSERT INTO FirstLook.StockTableDemoOne (ClientID, BrokerID, Symbol, TransactionType, TransactionDate, Quantity, Price, CommmissionRate) 
+   VALUES (92609349, 3103, 'HWVT', 'BUY',  '2017-10-25', 1500, 451.09, 3.25)
+GO
+INSERT INTO FirstLook.StockTableDemoOne (ClientID, BrokerID, Symbol, TransactionType, TransactionDate, Quantity, Price, CommmissionRate) 
+   VALUES (40149304, 5001, 'YQER', 'SELL',  '2015-10-13', 120, 105.30, 4.25)
+GO
+INSERT INTO FirstLook.StockTableDemoOne (ClientID, BrokerID, Symbol, TransactionType, TransactionDate, Quantity, Price, CommmissionRate) 
+   VALUES (29834783, 3103, 'HJQU', 'BUY',  '2017-07-23', 650, 110.41, 3.25)
+GO
+INSERT INTO FirstLook.StockTableDemoOne (ClientID, BrokerID, Symbol, TransactionType, TransactionDate, Quantity, Price, CommmissionRate) 
+   VALUES (85102938, 5001, 'WTI', 'SELL',  '2017-07-31', 350, 66.23, 4.25)
+GO
+INSERT INTO FirstLook.StockTableDemoOne (ClientID, BrokerID, Symbol, TransactionType, TransactionDate, Quantity, Price, CommmissionRate) 
+   VALUES (673845918, 1009, 'AJUI', 'BUY',  '2017-05-10', 10000, 75.04, 5.25)
+GO
+INSERT INTO FirstLook.StockTableDemoOne (ClientID, BrokerID, Symbol, TransactionType, TransactionDate, Quantity, Price, CommmissionRate) 
+   VALUES (810293023, 5001, 'ITBUY', 'BUY',  '2016-11-06', 2000, 110.34, 4.25)
+GO
+INSERT INTO FirstLook.StockTableDemoOne (ClientID, BrokerID, Symbol, TransactionType, TransactionDate, Quantity, Price, CommmissionRate) 
+   VALUES (73821029, 3103, 'MJTU', 'SELL',  '2016-10-31', 750, 60.02, 3.25)
+GO
+INSERT INTO FirstLook.StockTableDemoOne (ClientID, BrokerID, Symbol, TransactionType, TransactionDate, Quantity, Price, CommmissionRate) 
+   VALUES (810293023, 5001, 'ITUY', 'SELL',  '2017-11-06', 2000, 151.39, 4.25)
+GO
+INSERT INTO FirstLook.StockTableDemoOne (ClientID, BrokerID, Symbol, TransactionType, TransactionDate, Quantity, Price, CommmissionRate) 
+   VALUES (37482093, 1009, 'JKIO', 'BUY',  '2018-04-01', 500, 20.57, 5.25)
+GO
+INSERT INTO FirstLook.StockTableDemoOne (ClientID, BrokerID, Symbol, TransactionType, TransactionDate, Quantity, Price, CommmissionRate) 
+   VALUES (68101935, 1009, 'YTR', 'BUY',  '2016-01-17', 500, 35.92, 5.25)
+GO
+INSERT INTO FirstLook.StockTableDemoOne (ClientID, BrokerID, Symbol, TransactionType, TransactionDate, Quantity, Price, CommmissionRate) 
+   VALUES (87029367, 5001, 'HIO', 'BUY',  '2017-12-13', 200, 99.89, 4.25)
+GO
+INSERT INTO FirstLook.StockTableDemoOne (ClientID, BrokerID, Symbol, TransactionType, TransactionDate, Quantity, Price, CommmissionRate) 
+   VALUES (57293842, 5002, 'HIO', 'BUY',  '2017-12-15', 1000, 95.21, 5.5)
+GO
+INSERT INTO FirstLook.StockTableDemoOne (ClientID, BrokerID, Symbol, TransactionType, TransactionDate, Quantity, Price, CommmissionRate) 
+   VALUES (31306978, 3103, 'QURP', 'SELL',  '2017-09-24', 700, 82.35, 3.25)
+GO
+INSERT INTO FirstLook.StockTableDemoOne (ClientID, BrokerID, Symbol, TransactionType, TransactionDate, Quantity, Price, CommmissionRate) 
+   VALUES (57293842, 5002, 'HIO', 'SELL',  '2018-01-15', 1000, 92.35, 5.5)
+GO
